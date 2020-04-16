c5a234357df848b5dae05efd68fb8a8ab57bdad1
akshitsingla <akshit.singla@yahoo.com>
2020-04-15 22:11:23 -0400

    Adding dashboard with mock data, without styling

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This is the Chromecast (Receiver) app for COVID19-dashboard.
 
 # References
 - [assldev/helloworld-chromecast](https://github.com/assldev/helloworld-chromecast)
+- [COVID19 API](https://rapidapi.com/api-sports/api/covid-193)
+- [Google Charts Library](https://developers.google.com/chart/interactive/docs)
---
+++ sample-response.js
@@ -0,0 +1,154 @@
+var apiResponse = {
+  "get": "history",
+  "parameters": {
+    "day": "2020-04-05",
+    "country": "usa"
+  },
+  "errors": [],
+  "results": 8,
+  "response": [{
+    "country": "USA",
+    "cases": {
+      "new": "+280",
+      "active": 288355,
+      "critical": 8206,
+      "recovered": 14828,
+      "total": 311637
+    },
+    "deaths": {
+      "new": "+2",
+      "total": 8454
+    },
+    "tests": {
+      "total": 1656897
+    },
+    "day": "2020-04-05",
+    "time": "2020-04-05T08:45:05+00:00"
+  }, {
+    "country": "USA",
+    "cases": {
+      "new": "+280",
+      "active": 288355,
+      "critical": 8206,
+      "recovered": 14828,
+      "total": 311637
+    },
+    "deaths": {
+      "new": "+2",
+      "total": 8454
+    },
+    "tests": {
+      "total": null
+    },
+    "day": "2020-04-05",
+    "time": "2020-04-05T08:15:04+00:00"
+  }, {
+    "country": "USA",
+    "cases": {
+      "new": "+280",
+      "active": 288358,
+      "critical": 8206,
+      "recovered": 14825,
+      "total": 311637
+    },
+    "deaths": {
+      "new": "+2",
+      "total": 8454
+    },
+    "tests": {
+      "total": null
+    },
+    "day": "2020-04-05",
+    "time": "2020-04-05T06:15:05+00:00"
+  }, {
+    "country": "USA",
+    "cases": {
+      "new": "+278",
+      "active": 288356,
+      "critical": 8206,
+      "recovered": 14825,
+      "total": 311635
+    },
+    "deaths": {
+      "new": "+2",
+      "total": 8454
+    },
+    "tests": {
+      "total": null
+    },
+    "day": "2020-04-05",
+    "time": "2020-04-05T06:00:04+00:00"
+  }, {
+    "country": "USA",
+    "cases": {
+      "new": "+34196",
+      "active": 288080,
+      "critical": 8206,
+      "recovered": 14825,
+      "total": 311357
+    },
+    "deaths": {
+      "new": "+1048",
+      "total": 8452
+    },
+    "tests": {
+      "total": null
+    },
+    "day": "2020-04-05",
+    "time": "2020-04-05T03:30:05+00:00"
+  }, {
+    "country": "USA",
+    "cases": {
+      "new": "+34196",
+      "active": 288164,
+      "critical": 8206,
+      "recovered": 14741,
+      "total": 311357
+    },
+    "deaths": {
+      "new": "+1048",
+      "total": 8452
+    },
+    "tests": {
+      "total": null
+    },
+    "day": "2020-04-05",
+    "time": "2020-04-05T01:00:05+00:00"
+  }, {
+    "country": "USA",
+    "cases": {
+      "new": "+33125",
+      "active": 287093,
+      "critical": 8206,
+      "recovered": 14741,
+      "total": 310286
+    },
+    "deaths": {
+      "new": "+1048",
+      "total": 8452
+    },
+    "tests": {
+      "total": null
+    },
+    "day": "2020-04-05",
+    "time": "2020-04-05T00:15:04+00:00"
+  }, {
+    "country": "USA",
+    "cases": {
+      "new": "+33053",
+      "active": 287029,
+      "critical": 8206,
+      "recovered": 14741,
+      "total": 310214
+    },
+    "deaths": {
+      "new": "+1040",
+      "total": 8444
+    },
+    "tests": {
+      "total": null
+    },
+    "day": "2020-04-05",
+    "time": "2020-04-05T00:00:05+00:00"
+  }]
+}
